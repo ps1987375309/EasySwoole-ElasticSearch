645d4a1d431a11b567c6b9dffa0c11e839833fc6
艾鹏 <1987375309@qq.com>
2020-06-19 19:06:00 +0800

    提交测试

---
+++ EasySwooleEvent.php
@@ -0,0 +1,47 @@
+<?php
+namespace EasySwoole\EasySwoole;
+
+use EasySwoole\EasySwoole\Swoole\EventRegister;
+use EasySwoole\EasySwoole\AbstractInterface\Event;
+use EasySwoole\Http\Request;
+use EasySwoole\Http\Response;
+
+use EasySwoole\Component\Di;
+use App\Lib\Redis\Redis;
+use App\Lib\Process\ConsumerTest;
+use EasySwoole\Component\Process\Manager;
+
+class EasySwooleEvent implements Event
+{
+
+    public static function initialize()
+    {
+        // TODO: Implement initialize() method.
+        date_default_timezone_set('Asia/Shanghai');
+    }
+
+    public static function mainServerCreate(EventRegister $register)
+    {
+        // TODO: Implement mainServerCreate() method.
+        
+        Di::getInstance()->set('REDIS', Redis::getInstance());    //注册REDIS，下次其他地方就可以直接调用，就相当于REDIS = Redis::getInstance()
+        
+        $allNum = 3;
+        for ($i = 0 ;$i < $allNum;$i++){
+            $processConfig= new \EasySwoole\Component\Process\Config();
+            $processConfig->setProcessName('ConsumerTest'.$i);//设置进程名称
+            Manager::getInstance()->addProcess(new ConsumerTest($processConfig));
+        }
+    }
+
+    public static function onRequest(Request $request, Response $response): bool
+    {
+        // TODO: Implement onRequest() method.
+        return true;
+    }
+
+    public static function afterRequest(Request $request, Response $response): void
+    {
+        // TODO: Implement afterAction() method.
+    }
+}
